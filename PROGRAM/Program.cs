9928e856dd3c7163a87f0f6b6a571747448fe826
Sokolov Ilya <sokolov.ilia.2000@mail.ru>
2023-09-18 19:59:03 +0300

    folder (PROGRAM) added

---
+++ PROGRAM/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = { "hello", "2", "world", ":-)", "gg" };
+int index = 0;
+int count = 3;
+
+while (index < array.Length)
+{
+    string str = array[index];
+    char[] arr = str.ToCharArray();
+    int i = 0;
+    if (arr.Length <= count)
+    {
+        string result = new string(arr);
+        System.Console.Write("[" + string.Join(", ", result) + "]");
+        array[i]=result;
+        i++;
+        index++;
+    }
+    else
+    {
+        index++;
+    }
+    
+    
+}
